c07b759ddae01e194de342d2e69610539bb38a29
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-10 22:31:13 -0500

    updated

---
+++ main.js
@@ -0,0 +1,13 @@
+const result1 = yell('Hello')
+console.log(result1);
+const result2 = yell('Hi');
+result2;
+
+function getFirstCharacter(str) {
+    return str[0]
+}
+
+function getLastCharacter(str) {
+    return str[str.length - 1];
+}
+
